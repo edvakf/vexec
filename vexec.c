37e4013206016f72a0302fbd6d75aa42736e71ad
edvakf <edvakf@pixiv.com>
2013-12-15 21:46:48 +0900

    fork & exec

--- vexec.c
+++ vexec.c
@@ -1,16 +1,55 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <errno.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        fprintf(stderr, "please give at least one arg");
+        fprintf(stderr, "please pass at least one argument");
         return -1;
     }
 
     char **command = &argv[1];
 
-    execvp(command[0], command);
+    pid_t pid;
 
-    perror(command[0]);
-    return -1;
+    pid = fork();
+
+    if (pid < 0) {
+        perror("fork");
+        return errno;
+    }
+
+    if (pid == 0) {
+        // child process
+        execvp(command[0], command);
+
+        // comes here only when failed
+        perror(command[0]);
+        return errno;
+    }
+
+    // parent process
+    int status;
+    int w = waitpid(pid, &status, 0);
+
+    if (w == -1) {
+        perror("waitpid");
+        return errno;
+    }
+
+    if (WIFEXITED(status)) {
+        printf("finished\n");
+        return 0;
+    }
+
+    if (WIFSIGNALED(status)) {
+        printf("finished by signal %d\n", WTERMSIG(status));
+        return 0;
+    }
+
+    printf("exited\n");
+    return WEXITSTATUS(status);
+
 }
+
