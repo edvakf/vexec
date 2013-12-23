53e30c9ab3a82bcf7aae7b2b5a7a50fca5eece1f
edvakf <edvakf@pixiv.com>
2013-12-23 23:37:37 +0900

    use libev instead of pthread

--- vexec.c
+++ vexec.c
@@ -1,12 +1,11 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
-#include <sys/wait.h>
-#include <pthread.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <ev.h>
 
-#define BUFSIZE 8
+#define BUFSIZE 256
 
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
@@ -16,125 +15,46 @@
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
-typedef enum {
-    VEXEC_MODE_OUT,
-    VEXEC_MODE_ERR,
-    VEXEC_MODE_NEITHER,
-} vexec_mode;
-
-typedef struct vexec_buflist_item {
-    char buf[BUFSIZE];
-    size_t size;
-    struct vexec_buflist_item *next;
-    vexec_mode mode;
-} vexec_buflist_item_t;
-
-typedef struct {
-    vexec_buflist_item_t *head;
-    vexec_buflist_item_t *current;
-    vexec_buflist_item_t *tail;
-} vexec_buflist_t;
-
-vexec_buflist_item_t *alloc_vexec_buflist_item() {
-    return (vexec_buflist_item_t *)malloc(sizeof(vexec_buflist_item_t));
-}
-
-typedef struct {
-    int child_out;
-    int child_err;
-    vexec_buflist_t buflist;
-    pthread_rwlock_t lock;
-    int read_status;
-} vexec_env_t;
-
-void pipe_reader(vexec_env_t *env, int pipe, vexec_mode mode) {
-    while (1) {
-        pthread_rwlock_wrlock(&env->lock);
-        ssize_t size = read(pipe, env->buflist.current->buf, BUFSIZE);
-        if (size == 0) {
-            env->read_status--;
-            pthread_rwlock_unlock(&env->lock);
-            break;
-        } else if (size < 0) {
-            if (EAGAIN == errno || EWOULDBLOCK == errno) {
-                pthread_rwlock_unlock(&env->lock);
-                continue;
-            }
-            fprintf(stderr, "fread failed\n");
-            env->read_status--;
-            pthread_rwlock_unlock(&env->lock);
-            break;
+void child_out_readable_cb(struct ev_loop *loop, ev_io *w, int revents) {
+    char *buf[BUFSIZE];
+    ssize_t size = read(w->fd, buf, BUFSIZE);
+    if (size == 0) {
+        ev_io_stop(loop, w);
+        return;
+    } else if (size < 0) {
+        if (EAGAIN == errno || EWOULDBLOCK == errno) {
+            return;
         }
-        env->buflist.current->size = (size_t)size;
-        env->buflist.current->mode = mode;
-        if (env->buflist.current == env->buflist.tail) {
-            env->buflist.tail = env->buflist.current->next = alloc_vexec_buflist_item();
+        fprintf(stderr, "fread failed\n");
+        ev_io_stop(loop, w);
+        return;
+    }
+    fprintf(stdout, ANSI_COLOR_GREEN);
+    fwrite(buf, size, 1, stdout);
+    fprintf(stdout, ANSI_COLOR_RESET);
+}
+
+void child_err_readable_cb(struct ev_loop *loop, ev_io *w, int revents) {
+    char *buf[BUFSIZE];
+    ssize_t size = read(w->fd, buf, BUFSIZE);
+    if (size == 0) {
+        ev_io_stop(loop, w);
+        return;
+    } else if (size < 0) {
+        if (EAGAIN == errno || EWOULDBLOCK == errno) {
+            return;
         }
-        env->buflist.current = env->buflist.current->next;
-        pthread_rwlock_unlock(&env->lock);
+        fprintf(stderr, "fread failed\n");
+        ev_io_stop(loop, w);
+        return;
     }
+    fprintf(stdout, ANSI_COLOR_RED);
+    fwrite(buf, size, 1, stdout);
+    fprintf(stdout, ANSI_COLOR_RESET);
 }
 
-void *cb_out_reader(void *arg) {
-    vexec_env_t *env = (vexec_env_t *)arg;
-    int pipe = env->child_out;
-    pipe_reader(env, pipe, VEXEC_MODE_OUT);
-    return NULL;
-}
-
-void *cb_err_reader(void *arg) {
-    vexec_env_t *env = (vexec_env_t *)arg;
-    int pipe = env->child_err;
-    pipe_reader(env, pipe, VEXEC_MODE_ERR);
-    return NULL;
-}
-
-void *cb_writer(void *arg) {
-    vexec_env_t *env = (vexec_env_t *)arg;
-    vexec_mode mode = VEXEC_MODE_NEITHER;
-    while (1) {
-        pthread_rwlock_wrlock(&env->lock);
-        while (env->buflist.current != env->buflist.head) {
-            if (mode != env->buflist.head->mode) {
-                if (VEXEC_MODE_OUT == env->buflist.head->mode) {
-                    printf("%s", ANSI_COLOR_GREEN);
-                } else if (VEXEC_MODE_ERR == env->buflist.head->mode) {
-                    printf("%s", ANSI_COLOR_RED);
-                }
-                mode = env->buflist.head->mode;
-            }
-            fwrite(env->buflist.head->buf, env->buflist.head->size, 1, stdout);
-            env->buflist.tail = env->buflist.tail->next = env->buflist.head;
-            env->buflist.head = env->buflist.head->next;
-            env->buflist.tail->next = NULL;
-        }
-        if (0 == env->read_status) {
-            pthread_rwlock_unlock(&env->lock);
-            break;
-        }
-        pthread_rwlock_unlock(&env->lock);
-        usleep(100000);
-    }
-    printf("%s", ANSI_COLOR_RESET);
-    return NULL;
-}
-
-int wait_for_child(pid_t pid) {
-    int status;
-
-    if (-1 == waitpid(pid, &status, 0)) {
-        perror("waitpid");
-        return -1;
-    }
-    if (WIFEXITED(status)) {
-        return 0;
-    }
-    if (WIFSIGNALED(status)) {
-        return 0;
-    }
-
-    fprintf(stderr, "unexpectedly exited\n");
-    return -1;
+void child_status_change_cb(struct ev_loop *loop, ev_child *w, int revents) {
+    ev_child_stop(loop, w);
 }
 
 int main(int argc, char *argv[]) {
@@ -184,32 +104,21 @@ int main(int argc, char *argv[]) {
     fcntl(child_err, F_SETFL, fcntl(child_err, F_GETFL) | O_NONBLOCK);
     close(fileno(stdin)); // stdin will be read by the child process
 
-    pthread_t th_out_reader, th_err_reader, th_writer;
+    struct ev_loop *loop = ev_default_loop(0);
 
-    vexec_env_t env;
-    env.child_out = child_out;
-    env.child_err = child_err;
-    env.buflist.head = env.buflist.tail = env.buflist.current = alloc_vexec_buflist_item();
-    env.read_status = 2;
-    pthread_rwlock_init(&env.lock, NULL);
+    ev_io child_out_readable, child_err_readable;
+    ev_child child_status_change;
 
-    if (-1 == pthread_create(&th_out_reader, NULL, cb_out_reader, &env) ||
-        -1 == pthread_create(&th_err_reader, NULL, cb_err_reader, &env) ||
-        -1 == pthread_create(&th_writer, NULL, cb_writer, &env)) {
-        fprintf(stderr, "pthread_create failed\n");
-        return -1;
-    }
+    ev_io_init(&child_out_readable, child_out_readable_cb, child_out, EV_READ);
+    ev_io_init(&child_err_readable, child_err_readable_cb, child_err, EV_READ);
+    ev_child_init(&child_status_change, child_status_change_cb, pid, 0);
 
-    if (-1 == wait_for_child(pid)) {
-        return -1;
-    }
-    if (0 != pthread_join(th_out_reader, NULL) ||
-        0 != pthread_join(th_err_reader, NULL) ||
-        0 != pthread_join(th_writer, NULL)) {
-        fprintf(stderr, "could not properly join threads");
-        return -1;
-    }
-    printf("%s", ANSI_COLOR_RESET);
+    ev_io_start(loop, &child_out_readable);
+    ev_io_start(loop, &child_err_readable);
+    ev_child_start(loop, &child_status_change);
+
+    ev_run(loop, 0);
+
     return 0;
 }
 
