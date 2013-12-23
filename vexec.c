253a2fa5a01f5603768af2ac9071d4056f471ab9
edvakf <edvakf@pixiv.com>
2013-12-24 03:04:05 +0900

    output handling is also done in event loop

--- vexec.c
+++ vexec.c
@@ -21,48 +21,60 @@ typedef enum {
     VEXEC_MODE_NEITHER,
 } vexec_mode;
 
-typedef struct {
+typedef struct vexec_buflist_item {
+    char buf[BUFSIZE];
+    size_t size;
+    struct vexec_buflist_item *next;
     vexec_mode mode;
+} vexec_buflist_item_t;
+
+typedef struct {
+    vexec_buflist_item_t *head;
+    vexec_buflist_item_t *current;
+    vexec_buflist_item_t *tail;
+} vexec_buflist_t;
+
+vexec_buflist_item_t *alloc_vexec_buflist_item() {
+    return (vexec_buflist_item_t *)malloc(sizeof(vexec_buflist_item_t));
+}
+
+typedef struct {
     int exit_status;
+    vexec_buflist_t buflist;
+    int number_of_watchers;
 } vexec_env_t;
 
 void handle_child_readable(
-        struct ev_loop *loop, ev_io *w, int revents, vexec_mode mode, const char *esc) {
+        struct ev_loop *loop, ev_io *w, int revents, vexec_mode mode) {
     vexec_env_t *env = (vexec_env_t *)ev_userdata(loop);
-    char *buf[BUFSIZE];
-    ssize_t size = read(w->fd, buf, BUFSIZE);
+    ssize_t size = read(w->fd, env->buflist.current->buf, BUFSIZE);
     if (size == 0) {
-        if (env->mode != VEXEC_MODE_NEITHER) {
-            fprintf(stdout, ANSI_COLOR_RESET);
-            env->mode = VEXEC_MODE_NEITHER;
-        }
         ev_io_stop(loop, w);
+        env->number_of_watchers--;
         return;
     } else if (size < 0) {
         if (EAGAIN == errno || EWOULDBLOCK == errno) {
             return;
         }
-        if (env->mode != VEXEC_MODE_NEITHER) {
-            fprintf(stdout, ANSI_COLOR_RESET);
-            env->mode = VEXEC_MODE_NEITHER;
-        }
         fprintf(stderr, "fread failed\n");
         ev_io_stop(loop, w);
+        env->number_of_watchers--;
         return;
     }
-    if (env->mode != mode) {
-        fprintf(stdout, "%s", esc);
-        env->mode = mode;
+    env->buflist.current->size = (size_t)size;
+    env->buflist.current->mode = mode;
+    if (env->buflist.current == env->buflist.tail) {
+        env->buflist.tail = env->buflist.current->next = alloc_vexec_buflist_item();
     }
-    fwrite(buf, size, 1, stdout);
+    env->buflist.current = env->buflist.current->next;
 }
 
 void child_out_readable_cb(struct ev_loop *loop, ev_io *w, int revents) {
-    handle_child_readable(loop, w, revents, VEXEC_MODE_OUT, ANSI_COLOR_GREEN);
+    handle_child_readable(loop, w, revents, VEXEC_MODE_OUT);
 }
 
 void child_err_readable_cb(struct ev_loop *loop, ev_io *w, int revents) {
-    handle_child_readable(loop, w, revents, VEXEC_MODE_ERR, ANSI_COLOR_RED);
+    handle_child_readable(loop, w, revents, VEXEC_MODE_ERR);
 }
 
 void child_status_change_cb(struct ev_loop *loop, ev_child *w, int revents) {
@@ -76,6 +88,32 @@ void child_status_change_cb(struct ev_loop *loop, ev_child *w, int revents) {
         env->exit_status = 1;
     }
     ev_child_stop(loop, w);
+    env->number_of_watchers--;
+}
+
+void clear_buflist_cb(struct ev_loop *loop, ev_periodic *w, int revents) {
+    vexec_env_t *env = (vexec_env_t *)ev_userdata(loop);
+    vexec_mode mode = VEXEC_MODE_NEITHER;
+    while (env->buflist.current != env->buflist.head) {
+        if (mode != env->buflist.head->mode) {
+            if (VEXEC_MODE_OUT == env->buflist.head->mode) {
+                printf("%s", ANSI_COLOR_GREEN);
+            } else if (VEXEC_MODE_ERR == env->buflist.head->mode) {
+                printf("%s", ANSI_COLOR_RED);
+            }
+            mode = env->buflist.head->mode;
+        }
+        fwrite(env->buflist.head->buf, env->buflist.head->size, 1, stdout);
+        env->buflist.tail = env->buflist.tail->next = env->buflist.head;
+        env->buflist.head = env->buflist.head->next;
+        env->buflist.tail->next = NULL;
+    }
+    if (1 == env->number_of_watchers) {
+        // child process died, all out & err buffers cleared
+        printf("%s", ANSI_COLOR_RESET);
+        ev_periodic_stop(loop, w);
+        env->number_of_watchers--;
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -129,19 +167,24 @@ int main(int argc, char *argv[]) {
 
     vexec_env_t env;
     env.exit_status = 0;
-    env.mode = VEXEC_MODE_NEITHER;
+    env.buflist.head = env.buflist.tail = env.buflist.current = alloc_vexec_buflist_item();
+    env.number_of_watchers = 4;
     ev_set_userdata(loop, &env);
 
     ev_io child_out_readable, child_err_readable;
     ev_child child_status_change;
+    ev_periodic clear_buflist;
 
     ev_io_init(&child_out_readable, child_out_readable_cb, child_out, EV_READ);
     ev_io_init(&child_err_readable, child_err_readable_cb, child_err, EV_READ);
     ev_child_init(&child_status_change, child_status_change_cb, pid, 0);
+    ev_periodic_init(&clear_buflist, clear_buflist_cb, 0.1, 0.1, 0);
+    ev_set_priority(&clear_buflist, -1);
 
     ev_io_start(loop, &child_out_readable);
     ev_io_start(loop, &child_err_readable);
     ev_child_start(loop, &child_status_change);
+    ev_periodic_start(loop, &clear_buflist);
 
     ev_run(loop, 0);
 
